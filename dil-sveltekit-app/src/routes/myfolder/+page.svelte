769f144cbf59aabe9f8ac569c3bd6bd771a92f56
Lotus <niroshifinance@gmail.com>
2024-07-12 06:58:55 +0100

    before and after image in swipe folder

---
+++ dil-sveltekit-app/src/routes/myfolder/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  // Path to the image file
+  const imagePath = '/cake.jpeg'; 
+</script>
+<div class="circular-image">
+  <img src={imagePath} alt="Circular Image" />
+</div>
+<style>
+  .circular-image {
+    width: 200px; 
+    height: 200px;
+    border-radius: 50%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .circular-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
+
+
